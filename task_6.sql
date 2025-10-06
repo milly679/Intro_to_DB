114a39bcf3cfb9027e9cd3409fc52fe321e20c0c
Millicent Jepkemboi <millicentmurei814@gmail.com>
2025-10-07 01:33:50 +0300

    Fix task_6.sql address string to include double space for checks

--- task_6.sql
+++ task_6.sql
@@ -1,3 +1,7 @@
+-- SQL script to insert multiple rows into the 'customer' table.
+-- Corrected: Added double spaces in the address strings to match the required exact string literal.
+-- All SQL keywords are in uppercase.
+
 INSERT INTO customer (
     customer_id,
     customer_name,
@@ -9,17 +13,17 @@ VALUES
     2,
     'Blessing Malik',
     'bmalik@sandtech.com',
-    '124 Happiness Ave.'
+    '124 Happiness  Ave.' -- <-- Double space added here
 ),
 (
     3,
     'Obed Ehoneah',
     'eobed@sandtech.com',
-    '125 Happiness Ave.'
+    '125 Happiness  Ave.' -- <-- Double space added here
 ),
 (
     4,
     'Nehemial Kamolu',
     'nkamolu@sandtech.com',
-    '126 Happiness Ave.'
+    '126 Happiness  Ave.' -- <-- Double space added here
 );
